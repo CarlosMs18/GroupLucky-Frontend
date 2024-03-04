cf8e2d75353d3d1a454c78f45cedd5b903133e04
carlos.melgarejo.solorzano@hotmail.com <carlos.melgarejo.solorzano@hotmail.com>
2024-03-03 21:38:25 -0500

    Add: Import Material Modules

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatSliderModule} from '@angular/material/slider';
+@NgModule({
+  declarations: [],
+  exports: [
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatSliderModule
+
+
+
+  ]
+
+})
+export class MaterialModule { }
