80b8dc843ced46bb1a7bc17bfd7d30bbfe94f82b
carlos.melgarejo.solorzano@hotmail.com <carlos.melgarejo.solorzano@hotmail.com>
2024-03-04 15:46:33 -0500

    Add: ProductList in table

--- src/app/core/src/services/products/products.service.ts
+++ src/app/core/src/services/products/products.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Product } from '../../models/product/product.model';
 
+const API_PRODUCTS_URL = `${environment.apiUrl}/Product`;
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getProductAll(): Observable<Product[]>{
+    return this.http.get<Product[]>(`${API_PRODUCTS_URL}/GetProductsAll`);
+  }
 }
--- src/app/grouplucky/products/product-list/product-list.component.ts
+++ src/app/grouplucky/products/product-list/product-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ProductsService } from 'src/app/core';
+import { Product } from 'src/app/core/src/models/product/product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  displayedColumns: string[] = ['Id', 'Nombre del Producto', 'Stock Mínimo', 'Stock Máximo', 'Precio de Venta', 'Categoría', 'Estado', 'Acciones'];
-  constructor() { }
+ /*  displayedColumns: string[] = ['id','code','name', 'stockmin', 'stockmax', 'unitsalePrice', 'categoria', 'estado', 'Acciones']; */
+  displayedColumns: string[] = ['id','code','name','stockMin','stockMax','unitSalePrice','categoria','estado','acciones']
+  dataSource =new MatTableDataSource<Product>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(
+    private _productService :ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.getProductsAll();
   }
 
+  getProductsAll(){
+    this._productService.getProductAll().subscribe(products =>{
+      console.log(products)
+      this.dataSource.data = products
+    })
+  }
+
+  addProduct(){
+
+  }
 }
