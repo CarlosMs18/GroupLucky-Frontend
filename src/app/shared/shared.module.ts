7aaa7411f63868b498c7e1ae5ecb0af89c2b983d
carlos.melgarejo.solorzano@hotmail.com <carlos.melgarejo.solorzano@hotmail.com>
2024-03-04 19:16:53 -0500

    Delete: Sidenav component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MaterialModule } from '../material/material.module';
-import { SidenavComponent } from './sidenav/sidenav.component';
 import { SearchFieldComponent } from './search-field/search-field.component';
 import { TableComponent } from './table/table.component';
 import { NavListComponent } from './nav-list/nav-list.component';
@@ -13,14 +12,12 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     ToolbarComponent,
-    SidenavComponent,
     SearchFieldComponent,
     TableComponent,
     NavListComponent
   ],
   exports:[
     ToolbarComponent,
-    SidenavComponent,
     SearchFieldComponent,
     TableComponent,
     NavListComponent
